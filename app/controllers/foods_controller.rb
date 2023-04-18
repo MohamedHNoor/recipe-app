be80264f01e858bfe51386feaa9e6aa4989fc04d
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-04-18 15:04:25 +0200

    Add methods to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,33 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+  
+    
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+  
+    return unless @food.save
+    redirect_to foods_path, notice: 'Food was successfully created.'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
