2b68d0c8ee9da1c9e2ae71ae5eec9ff331fe7378
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-21 14:23:14 +0200

    Remove comments and remove the id from shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root to: "foods#index"
-
-  # get 'recipe_foods/:recipe_id', to: 'recipe_foods#new', as: 'new_recipe_food'
-  # post 'recipe_foods/:recipe_id', to: 'recipe_foods#create', as: 'create_recipe_food'
   
   resources :users, only: [:index, :show]  do
-    resources :foods
-    resources :recipes do
-      resources :recipe_foods
+    resources :foods, only: [:index, :new, :create, :destroy]
+    resources :recipes, only:[:index, :show, :new, :create, :destroy] do
+      resources :recipe_foods, only: [:index, :show, :new, :create, :destroy]
     end
   end
 
-  # post '/users/:user_id/recipes/:recipe_id/recipe_foods', to: 'recipe_foods#create', as: 'create_recipe_recipe_food'
-  get 'shopping_list/:recipe_id', to: 'recipes#shopping_list', as: 'shopping_list'
-  get 'public_recipes', to: 'recipes#public_recipes', as: 'public_recipes'
+  get '/shopping_list', to: 'shopping_list#index', as: 'shopping_list'
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
 
-  # Defines the root path route ("/")
 end
